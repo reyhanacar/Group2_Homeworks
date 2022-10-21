51a22091613cc79bb1265d5ca75a53243eab671b
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-21 19:23:49 +0200

    Dortgen has been solved.

--- src/day11/Dortgen.java
+++ src/day11/Dortgen.java
@@ -1,21 +1,38 @@
 package day11;
 
+import java.util.Scanner;
+
 public class Dortgen {
     public static void main(String[] args) {
-        //TODO: Dortgen programını geliştiriniz.
-        /*
-            1. Klavyeden 3 adet girdi alınacaktır.
-                kenar:int       -> Karenin kenarı
-                kisaKenar:int   -> Dikdörtgenin kısa kenarı
-                uzunKenar:int   -> Dikdörtgenin uzun kenarı
-            2. alanHesapla(int k1, int k2):int  -> Metodu geliştirilecektir.
-            3. Tek bir metot (alanHesapla) iki kez çağırılarak hem karenin hem de
-            dikdörtgenin alanı hesaplanacaktır.
-            4. cevreHesapla(int k1,int k2):int  -> Metodu geliştirilecektir.
-            5. Tek bir metot (cevreHesapla) iki kez çağırılarak hem karenin hem de
-            dikdörtgenin çevresi hesaplanacaktır.
-            6. Elde edilen toplam alan hesaplanıp ekranda gösterilecektir.
-            7. Elde edilen toplam çevre ekranda gösterilecektir.
-         */
+        Scanner input=new Scanner(System.in);
+        System.out.print("Kare için kenar : ");
+        int kenar = input.nextInt();
+        System.out.print("Dikdörtgen için Kısa keanr : ");
+        int kisaKenar = input.nextInt();
+        System.out.print("Dikdörtgen için Uzun kenar : ");
+        int uzunKenar= input.nextInt();
+
+        int kareninAlani=alanHesapla(kenar,kenar);
+        int kareninCevresi=cevreHesapla(kenar,kenar);
+
+        int dikdortgeninAlani=alanHesapla(kisaKenar,uzunKenar);
+        int dikdortgeninCevresi=cevreHesapla(kisaKenar,uzunKenar);
+
+        int toplamAlan=kareninAlani+dikdortgeninAlani;
+        int toplamCevre=kareninCevresi+dikdortgeninCevresi;
+
+        System.out.println("Toplam Alan = "+toplamAlan);
+        System.out.println("Toplam Çevre = "+toplamCevre);
+
+
     }
+
+    public static int alanHesapla(int k1,int k2){
+        return k1*k2;
+    }
+
+    public static int cevreHesapla(int k1,int k2){
+        return 2*(k1+k2);
+    }
+
 }
---
+++ src/day11/Islemler.java
@@ -0,0 +1,10 @@
+package day11;
+
+public class Islemler {
+    public static void main(String[] args) {
+        int alan = Dortgen.alanHesapla(7, 10);
+        int cevre = Dortgen.cevreHesapla(7, 10);
+
+        System.out.printf("Alan = %d\tÇevre= %d\n",alan,cevre);
+    }
+}
