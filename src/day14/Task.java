fb542c52678338eec4dd2cab79f1b1ebf4fa2a79
Gürhan Ustali <gurhanustali@gmail.com>
2022-10-24 21:57:15 +0200

    Swap

---
+++ src/day14/Task.java
@@ -0,0 +1,11 @@
+package day14;
+
+public class Task {
+    public static void main(String[] args) {
+        //TODO: İki tamsayı değişkenine değerleri assign ediniz.
+        /*
+            Önce bunları isimleri ve değerleriyle birlikte ekrana yzadırınız.
+            Daha sonra iki değişkenin değerlerini yer değiştirip ekrana tekrar yazdırınız.
+         */
+    }
+}
